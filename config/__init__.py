b61a133a33c84ca7d69361e9b3dfb3879ed73406
Loïc Baret <b.loic38@laposte.net>
2021-04-27 21:05:49 -0400

    Files initialization

---
+++ config/__init__.py
@@ -0,0 +1,7 @@
+from os.path import join, abspath, dirname
+
+ROOT = dirname(abspath(dirname(__name__)))
+
+DATA_DIR = join(ROOT, 'data')
+ANALYSE_DIR = join(ROOT, 'analyse')
+MODEL_DIR = join(ROOT, 'model')
