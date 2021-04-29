921eb40514377adbf925e64e42759a173b8b0fd9
Loïc Baret <b.loic38@laposte.net>
2021-04-28 22:46:36 -0400

    Configurations update

--- config/__init__.py
+++ config/__init__.py
@@ -6,4 +6,16 @@ DATA_DIR = join(ROOT, 'data')
 ANALYSE_DIR = join(ROOT, 'analyse')
 MODEL_DIR = join(ROOT, 'model')
 
-
+AIRBNB_FILES = {
+    'data': [
+        'listings.csv.gz',
+        'calendar.csv.gz',
+        'reviews.csv.gz'
+    ],
+    'visualisations': [
+        'listings.csv',
+        'reviews.csv',
+        'neighbourhoods.csv',
+        'neighbourhoods.geojson'
+    ]
+}
